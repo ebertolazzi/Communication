bbab18764d7a6afa6190d1a8d2f14648d24b4104
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2019-02-15 20:18:00 +0100

    updated xlsx_to_yaml

--- src_generator/xlsx_to_yaml.rb
+++ src_generator/xlsx_to_yaml.rb
@@ -30,6 +30,9 @@ objects[:origin_file] = File.basename conf.input_file_name
 
 puts conf.input_file_name
 
+$workbook_data = {}
+$last_struct   = {}
+
 workbook = RubyXL::Parser.parse(conf.input_file_name+".xlsx")
 
 workbook.worksheets[0].each_with_index do |data_row,lineNumber|
@@ -39,16 +42,31 @@ workbook.worksheets[0].each_with_index do |data_row,lineNumber|
     row << val;
   }
   case lineNumber
-  when 0 # Do nothing
-  when 1 # Select the column indexes of each field
+  when 0
+    #  ___             __
+    # | _ \_____ __ __/  \
+    # |   / _ \ V  V / () |
+    # |_|_\___/\_/\_/ \__/
+    #
+    version    = data_row[0].value
+    main_topic = data_row[1].value
+    puts "MAIN TOPIC=#{main_topic} #{version}"
+  when 1
+    #  _  _             _
+    # | || |___ __ _ __| |___ _ _
+    # | __ / -_) _` / _` / -_) '_|
+    # |_||_\___\__,_\__,_\___|_|
+    #
+    # Select the column indexes of each field
     tmp = []
     conf.columns_map.each do |item|
-      n     = item[0];
-      k     = item[1];
+      n = item[0];
+      k = item[1];
       tmp << n;
       idx = row.index(n)
       if idx then
         columns_to_idx[k] = idx
+        puts "Mapped column \##{idx} to `#{n}`".green
       else
         puts "Missing column `#{n}` in header".red
       end
@@ -56,30 +74,68 @@ workbook.worksheets[0].each_with_index do |data_row,lineNumber|
     row.each_with_index do |item,icol|
       idx = tmp.index(item)
       if not idx then
-        puts "Column \##{icol} `#{item}` not mapped".yellow
+        puts "Column \##{icol} with field `#{item}` not mapped".yellow
       end
     end
   else
+    #     _      _
+    #  __| |__ _| |_ __ _
+    # / _` / _` |  _/ _` |
+    # \__,_\__,_|\__\__,_|
+    #
     # Create the input and manoeuvre messages
     begin
       # Scenario message
       if row[columns_to_idx[:name]] then
-        out = {}
+        data = {}
         conf.columns_map.each do |item|
           n   = item[0];
           k   = item[1];
           idx = columns_to_idx[k];
           if idx then
-            out[k] = row[idx].to_s;
+            data[k] = row[idx].to_s;
           else
-            out[k] = "MISSING";
+            data[k] = "MISSING";
           end
         end
-        columns << out;
+        columns << data;
       end
     rescue => e
       puts "skipping unmappable row `#{e}`".red
       # skipping unmappable row
+    end
+    begin
+      case data[:yes_no_x]
+      when 'yes' || 'no'
+        if $last_struct[:active] then
+          tmp = {
+            :fields      => $last_struct[:fields],
+            :subtopic    => $last_struct[:subtopic]
+          };
+          tmp[:subsubtopic] = $last_struct[:subsubtopic] if $last_struct[:subsubtopic]
+          $workbook_data[ $last_struct[:struct_name] ] = tmp;
+        end
+        $last_struct               = {};
+        $last_struct[:struct_name] = data[:name];
+        $last_struct[:active]      = data[:yes_no_x] == 'yes';
+        m = data[:description_subtopic].match(/^(.*)\/\#\{(.*)\}$/)
+        if m then
+          $last_struct[:subtopic]    = m[1];
+          $last_struct[:subsubtopic] = m[2];
+        else
+          $last_struct[:subtopic] = data[:description_subtopic];
+        end
+
+        $last_struct[:fields] = [];
+
+      when 'x'
+        data.delete_if { |key, value| !conf.for_structs.include? key }
+        $last_struct[:fields] << data;
+      when ''
+        puts "NO DATA=#{data[:name]}".cyan
+      end
+    rescue => e
+      puts "skipping row `#{lineNumber}` due to `#{e}`".yellow
     end
   end
 end
@@ -88,7 +144,7 @@ end
 # Delete old files in temporary folder (if present)
 FileUtils.rm_f conf.yaml_file_name
 File.open( conf.yaml_file_name + ".yaml", 'w' ) do |f|
-  f.write(columns.to_yaml(:Indent => 8))
+  f.write($workbook_data.to_yaml(:Indent => 8))
 end
 
 puts "File #{conf.yaml_file_name}.yaml generated"
---
+++ src_generator/xlsx_to_yaml_config.rb
@@ -0,0 +1,27 @@
+class Config
+  attr_accessor :input_file_name,
+                :yaml_file_name,
+                :columns_map,
+                :for_structs
+
+  def initialize
+    @input_file_name = '../../signalmap_safestrip_interfaces_v1.0'
+    @yaml_file_name  = 'signalmap'
+
+    @columns_map = [
+      [ "Name",                 :name                 ],
+      [ "Description/subtopic", :description_subtopic ],
+      [ "Comment",              :comment              ],
+      [ "Num. Elements",        :size                 ],
+      [ "Type",                 :type                 ],
+      [ "Range",                :range                ],
+      [ "Unit",                 :unit                 ],
+      [ "yes/no/x",             :yes_no_x             ],
+      [ "Scenario",             :scenario             ],
+      [ "Manoeuvre",            :manoeuvre            ]
+    ]
+
+    @for_structs = [ :name, :comment, :size, :type, :range, :unit ]
+
+  end
+end
