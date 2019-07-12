d6f66a2f635307a8e148603fafaf13e5cc260ec0
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2019-07-12 12:50:11 +0200

    updated UDP generator

--- src_generator/generate_UDP_lib.rb
+++ src_generator/generate_UDP_lib.rb
@@ -188,24 +188,18 @@ def generate_simulink_header( tag, value )
 <%= to_SIMULINK_struct( @tag, @value ) %>
 
 /* print <%= @tag %> struct on stdio */
-extern
-void
-buffer_to_<%= @tag %>( uint8_t const buffer[], <%= @tag %> * S );
+extern void buffer_to_<%= @tag %>( uint8_t const buffer[], <%= @tag %> * S );
 
 /* serialize <%= @tag %> struct to buffer */
-extern
-void
-<%= @tag %>_to_buffer( <%= @tag %> const * S, uint8_t buffer[] );
+extern void <%= @tag %>_to_buffer( <%= @tag %> const * S, uint8_t buffer[] );
 
 /* get buffer and un-serialize to <%= @tag %> struct */
-extern
-void
-<%= @tag %>_print( <%= @tag %> const * S );
+extern void <%= @tag %>_print( <%= @tag %> const * S );
 
-<%= simulink_to_buffer(@tag, @value) %>
-<%= simulink_from_buffer(@tag, @value) %>
-<%= simulink_set_output_signal(@tag, @value) %>
-<%= simulink_set_input_signal(@tag, @value) %>
+extern void simulink_<%= @tag %>_to_buffer( SimStruct *S, uint8_t *buffer );
+extern void simulink_buffer_to_<%= @tag %>( uint8_t const * buffer, SimStruct * S);
+extern void simulink_<%= @tag %>_set_output_signal( SimStruct *S );
+extern void simulink_<%= @tag %>_set_input_signal( SimStruct *S );
 
 #ifdef __cplusplus
 };
@@ -218,7 +212,6 @@ void
   eruby.evaluate(context)
 end
 
- 
  ####  # #    # #    # #      # #    # #    #    #####   ####  #####  #   #
 #      # ##  ## #    # #      # ##   # #   #     #    # #    # #    #  # #
  ####  # # ## # #    # #      # # #  # ####      #####  #    # #    #   #
@@ -241,247 +234,33 @@ def generate_simulink_body( tag, value )
   extern "C" {
 #endif
 
-#define S_FUNCTION_LEVEL 2
-#define S_FUNCTION_NAME  <%= @tag %>
+<%= simulink_to_buffer(@tag, @value) %>
+<%= simulink_from_buffer(@tag, @value) %>
+<%= simulink_set_output_signal(@tag, @value) %>
+<%= simulink_set_input_signal(@tag, @value) %>
 
-/*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
-/* %%%-SFUNWIZ_defines_Changes_BEGIN --- EDIT HERE TO _END */
-#define NUM_INPUTS            1
-/* Input Port  0 */
-#define IN_PORT_0_NAME        payload // @@@@@@@@@@@@@@@@@@@@
-#define INPUT_0_WIDTH         1
-#define INPUT_DIMS_0_COL      1
-#define INPUT_0_DTYPE         real_T
-#define INPUT_0_COMPLEX       COMPLEX_NO
-#define IN_0_FRAME_BASED      FRAME_NO
-#define IN_0_BUS_BASED        1
-#define IN_0_BUS_NAME         <%= @tag %>
-#define IN_0_DIMS             1-D
-#define INPUT_0_FEEDTHROUGH   1
-#define IN_0_ISSIGNED         0
-#define IN_0_WORDLENGTH       8
-#define IN_0_FIXPOINTSCALING  1
-#define IN_0_FRACTIONLENGTH   9
-#define IN_0_BIAS             0
-#define IN_0_SLOPE            0.125
-
-#define NUM_OUTPUTS           1
-/* Output Port  0 */
-#define OUT_PORT_0_NAME       serialized_data  // @@@@@@@@@@@@@@@@@@@@
-#define OUTPUT_0_WIDTH        <%= @tag %>_size // @@@@@@@@@@@@@@@@@@@@
-#define OUTPUT_DIMS_0_COL     1
-#define OUTPUT_0_DTYPE        uint32_T
-#define OUTPUT_0_COMPLEX      COMPLEX_NO
-#define OUT_0_FRAME_BASED     FRAME_NO
-#define OUT_0_BUS_BASED       0
-#define OUT_0_BUS_NAME        
-#define OUT_0_DIMS            1-D
-#define OUT_0_ISSIGNED        1
-#define OUT_0_WORDLENGTH      8
-#define OUT_0_FIXPOINTSCALING 1
-#define OUT_0_FRACTIONLENGTH  3
-#define OUT_0_BIAS            0
-#define OUT_0_SLOPE           0.125
-
-#define NPARAMS               0
-
-#define SAMPLE_TIME_0         INHERITED_SAMPLE_TIME
-#define NUM_DISC_STATES       0
-#define DISC_STATES_IC        [0]
-#define NUM_CONT_STATES       0
-#define CONT_STATES_IC        [0]
-
-#define SFUNWIZ_GENERATE_TLC  0
-//#define SOURCEFILES           "__SFB__"  // @@@@@@@@@@@@@@@@@@@@
-#define SOURCEFILES
-#define PANELINDEX            8
-#define USE_SIMSTRUCT         1
-#define SHOW_COMPILE_STEPS    0
-#define CREATE_DEBUG_MEXFILE  0
-#define SAVE_CODE_ONLY        1
-#define SFUNWIZ_REVISION      3.0
-
-#include "simstruc.h"
-//#include "sfun_bus_structs.h" NON SERVE
-#include "codriver_interfaces_data_structs.h"
-
-/*
- * Code Generation Environment flag (simulation or standalone target).
- */
-
-static int_T isSimulationTarget;
-
-/* Macro used to check if Simulation mode is set to accelerator */
-
-// #define isBusDWorkPresent  ( ( ( !ssRTWGenIsCodeGen(S) || isSimulationTarget ) && !ssIsExternalSim(S) ) || ssIsRapidAcceleratorActive(S) )
-
-/*====================*
- * S-function methods *
- *====================*/
-/* Function: mdlInitializeSizes ===============================================
- * Abstract:
- *   Setup sizes of the various vectors.
- */
-
-static
 void
-mdlInitializeSizes(SimStruct *S) {
-
-  DECL_AND_INIT_DIMSINFO(inputDimsInfo);
-  DECL_AND_INIT_DIMSINFO(outputDimsInfo);
-  ssSetNumSFcnParams(S, NPARAMS);
-  if (ssGetNumSFcnParams(S) != ssGetSFcnParamsCount(S)) {
-    return; /* Parameter mismatch will be reported by Simulink */
-  }
-
-  //ssSetArrayLayoutForCodeGen(S, SS_COLUMN_MAJOR);
-
-  ssSetSimStateCompliance(S, USE_DEFAULT_SIM_STATE);
-
-  ssSetNumContStates(S, NUM_CONT_STATES);
-  ssSetNumDiscStates(S, NUM_DISC_STATES);
-
-  if (!ssSetNumInputPorts(S, NUM_INPUTS)) return;
-
-  /* Register <%= @tag %>_str datatype for Input port 0 */
-
-  #if defined(MATLAB_MEX_FILE)
-  if (ssGetSimMode(S) != SS_SIMMODE_SIZES_CALL_ONLY) {
-    DTypeId dataTypeIdReg;
-    ssRegisterTypeFromNamedObject(S, "<%= @tag %>", &dataTypeIdReg);
-    if(dataTypeIdReg == INVALID_DTYPE_ID) return;
-    ssSetInputPortDataType(S, 0, dataTypeIdReg);
-  }
-  #endif
-  ssSetInputPortWidth(S, 0, INPUT_0_WIDTH);
-  ssSetInputPortComplexSignal(S, 0, INPUT_0_COMPLEX);
-  ssSetInputPortDirectFeedThrough(S, 0, INPUT_0_FEEDTHROUGH);
-  ssSetInputPortRequiredContiguous(S, 0, 1); /*direct input signal access*/
-  ssSetBusInputAsStruct(S, 0, IN_0_BUS_BASED);
-  ssSetInputPortBusMode(S, 0, SL_BUS_MODE);
-
-  if (!ssSetNumOutputPorts(S, NUM_OUTPUTS)) return;
-  ssSetOutputPortWidth(S, 0, OUTPUT_0_WIDTH);
-  ssSetOutputPortDataType(S, 0, SS_UINT8);
-  ssSetOutputPortComplexSignal(S, 0, OUTPUT_0_COMPLEX);
-
-  ssSetNumPWork(S, 0);
-
-  ssSetNumSampleTimes(S, 1);
-  ssSetNumRWork(S, 0);
-  ssSetNumIWork(S, 0);
-  ssSetNumModes(S, 0);
-  ssSetNumNonsampledZCs(S, 0);
-
-  ssSetSimulinkVersionGeneratedIn(S, "9.2");
-
-  /* Take care when specifying exception free code - see sfuntmpl_doc.c */
-  ssSetOptions(S, (SS_OPTION_EXCEPTION_FREE_CODE |
-                   SS_OPTION_WORKS_WITH_CODE_REUSE));
-}
-
-/* Function: mdlInitializeSampleTimes =========================================
- * Abstract:
- *    Specifiy  the sample time.
- */
-static
-void
-mdlInitializeSampleTimes(SimStruct *S) {
-  ssSetSampleTime(S, 0, SAMPLE_TIME_0);
-  ssSetModelReferenceSampleTimeDefaultInheritance(S);
-  ssSetOffsetTime(S, 0, 0.0);
-}
-
-#define MDL_SET_INPUT_PORT_DATA_TYPE
-static
-void
-mdlSetInputPortDataType(SimStruct *S, int port, DTypeId dType) {
-  ssSetInputPortDataType(S, 0, dType);
-}
-
-#define MDL_SET_OUTPUT_PORT_DATA_TYPE
-static
-void
-mdlSetOutputPortDataType(SimStruct *S, int port, DTypeId dType) {
-  ssSetOutputPortDataType(S, 0, dType);
-}
-
-#define MDL_SET_DEFAULT_PORT_DATA_TYPES
-static
-void
-mdlSetDefaultPortDataTypes(SimStruct *S) {
-  ssSetInputPortDataType(S, 0, SS_DOUBLE);
-  ssSetOutputPortDataType(S, 0, SS_DOUBLE);
-}
-
-#define MDL_SET_WORK_WIDTHS
-#if defined(MDL_SET_WORK_WIDTHS) && defined(MATLAB_MEX_FILE)
-
-static
-void
-mdlSetWorkWidths(SimStruct *S) {
-}
-
-#endif
-
-#define MDL_START  /* Change to #undef to remove function */
-
-#if defined(MDL_START)
-/* Function: mdlStart =======================================================
- * Abstract:
- *    This function is called once at start of model execution. If you
- *    have states that should be initialized once, this is the place
- *    to do it.
- */
-static
-void
-mdlStart(SimStruct *S) {
-}
-#endif /*  MDL_START */
-
-/* Function: mdlOutputs =======================================================
- *
- */
-static
-void
-mdlOutputs(SimStruct *S, int_T tid) {
-  <%= to_SIMULINK_busInfo_in_data_rtw(@tag, @value) %>
-}
-
-/* Function: mdlTerminate =====================================================
- * Abstract:
- *    In this function, you should perform any actions that are necessary
- *    at the termination of a simulation.  For example, if memory was
- *    allocated in mdlStart, this is the place to free it.
- */
-static
-void
-mdlTerminate(SimStruct *S) {
-  /* Free stored bus information */
-}
-
-#if 0
-/* print <%= @tag %> struct on stdio */
-<%= to_SIMULINK_busInfo( @tag, @value ) %>
-
-/* serialize <%= @tag %> struct to buffer */
-<%= to_SIMULINK_busInfo_in_data( @tag, @value ) %>
-
-/* get buffer and un-serialize to <%= @tag %> struct */
+<%= @tag %>_mdlOutputs( SimStruct *S, int_T tid ) {
 <%= to_SIMULINK_busInfo_in_data_rtw( @tag, @value ) %>
+}
 
-/* get buffer and un-serialize to <%= @tag %> struct */
+void
+<%= @tag %>_busInfo( SimStruct *S ) {
+<%= to_SIMULINK_busInfo( @tag, @value ) %>
+}
+
+void
+<%= @tag %>_busInfo_in_data( SimStruct *S ) {
+<%= to_SIMULINK_busInfo_in_data( @tag, @value ) %>
+}
+
+void
+<%= @tag %>_message( SimStruct *S ) {
 <%= to_SIMULINK_message( @tag, @value ) %>
-#endif
+}
 
 #ifdef __cplusplus
 };
-#endif
-
-#ifdef  MATLAB_MEX_FILE    /* Is this file being compiled as a MEX-file? */
-#include "simulink.c"      /* MEX-file interface mechanism */
-#else
-#include "cg_sfun.h"       /* Code generation registration function */
 #endif
 
 '''
